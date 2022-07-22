65fb8d917801b7a9c68579024d23ab18a7eda6ba
toky <tokyfydev@gmail.com>
2022-07-22 14:01:11 +0300

    ADD : npm script ADD : 404 route handler

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-const morgan = require('morgan');
+import morgan from 'morgan';
 
 const FindsRoutes = require('./Routes/findRoute');
 
 const app = express();
 
+app.use(morgan("dev"))
+
 app.use('/api/rest/find', FindsRoutes);
 
+app.all('*' , (req , res)=>{
+    res.status(404).json({
+        message : "Our API Endpoints is at '/api/rest/find'!"
+    })
+})
 export default app;
